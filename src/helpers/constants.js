e5c2f81d5911b40f0586a6c69e219acd7b1866e3
Ramya-rem <ramyasubramaniam7301@gmail.com>
2024-12-31 10:26:12 +0530

    updating the levelup and threshholds

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,7 +1,7 @@
 // Level-up bonuses for each level
 const levelUpBonuses = [
-  10, // Level 2 to Level 3
-  20, // Level 3 to Level 4
+  1000, // Level 2 to Level 3
+  10000, // Level 3 to Level 4
   50000, // Level 4 to Level 5
   100000, // Level 5 to Level 6
   500000, // Level 6 to Level 7
@@ -13,9 +13,9 @@ const levelUpBonuses = [
 
 // Thresholds for level-ups based on totalRewards
 const thresholds = [
-  { limit: 0, rewardPerSecond: 1, level: 1 },
-  { limit: 600, rewardPerSecond: 2, level: 2 },
-  { limit: 800, rewardPerSecond: 3, level: 3 },
+  { limit: 500, rewardPerSecond: 1, level: 1 },
+  { limit: 10000, rewardPerSecond: 2, level: 2 },
+  { limit: 50000, rewardPerSecond: 3, level: 3 },
   { limit: 200000, rewardPerSecond: 4, level: 4 },
   { limit: 800000, rewardPerSecond: 5, level: 5 },
   { limit: 3000000, rewardPerSecond: 6, level: 6 },
