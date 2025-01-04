4da57f9c7baef3008a12c2b713cd9851f37b90d5
pavieshkumar12 <dhanushpavi22@gmail.com>
2025-01-04 19:04:16 +0530

    .env change

--- src/helpers/crypto.js
+++ src/helpers/crypto.js
@@ -1,26 +1,28 @@
 const CryptoJS = require('crypto-js');
+require('dotenv').config(); // Load environment variables from .env
 
-const secretKey = "mySecretKey12345"; // 16 characters for AES-128
-const iv = CryptoJS.enc.Utf8.parse("myInitVector1234"); // 16 characters for AES-128   // Use Utf8-Encoder.
+const secretKey = process.env.SECRET_KEY; // Retrieve the key from .env
+const iv = CryptoJS.enc.Utf8.parse("myInitVector1234"); // 16 characters for AES-128
 
- const decryptMessage = (encryptedMessage) => {
-    const decrypted = CryptoJS.AES.decrypt(encryptedMessage, CryptoJS.enc.Utf8.parse(secretKey), {
-      iv: iv,
-      mode: CryptoJS.mode.CBC,
-      padding: CryptoJS.pad.Pkcs7,
-    });
-    return decrypted.toString(CryptoJS.enc.Utf8);
-  };
+const decryptMessage = (encryptedMessage) => {
+  const decrypted = CryptoJS.AES.decrypt(encryptedMessage, CryptoJS.enc.Utf8.parse(secretKey), {
+    iv: iv,
+    mode: CryptoJS.mode.CBC,
+    padding: CryptoJS.pad.Pkcs7,
+  });
+  return decrypted.toString(CryptoJS.enc.Utf8);
+};
 
 const encryptMessage = (message) => {
-    const encrypted = CryptoJS.AES.encrypt(message, CryptoJS.enc.Utf8.parse(secretKey), {
-      iv: iv,
-      mode: CryptoJS.mode.CBC,
-      padding: CryptoJS.pad.Pkcs7,
-    });
-    const encryptedData =encrypted.toString();
-    const ivString =iv
-    return { encryptedData, ivString };
-  };
+  const encrypted = CryptoJS.AES.encrypt(message, CryptoJS.enc.Utf8.parse(secretKey), {
+    iv: iv,
+    mode: CryptoJS.mode.CBC,
+    padding: CryptoJS.pad.Pkcs7,
+  });
+  const encryptedData = encrypted.toString();
+  const ivString = iv;
+  return { encryptedData, ivString };
+};
 
-  module.exports ={decryptMessage, encryptMessage}
+module.exports = { decryptMessage, encryptMessage };
+
