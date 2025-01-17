7f12de272cfd3c78e1e55b530f503f3b47b0b050
pavieshkumar12 <dhanushpavi22@gmail.com>
2025-01-17 17:02:44 +0530

    dev-changes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ require('dotenv').config()
 const rateLimit = require('express-rate-limit')
 if (cluster.isMaster) {
   const token = process.env.TELEGRAM_TOKEN
-  const bot = new TelegramBot(token, { polling: true })
+  const bot = new TelegramBot(token)
 
   // Handle the /start command from Telegram
   bot.onText(/\/start(?:\s+(\w+))?/, (msg, match) => {
@@ -67,7 +67,7 @@ if (cluster.isMaster) {
     })
     .then(() => {
       logger.info(
-        '*********🛡️ 🔍  Successfully Connected to MongoDB 🛡️ 🔍 **********'
+        '*********🛡️ 🔍  Successfully Connected to MongoDB Stagging🛡️ 🔍 **********'
       )
     })
     .catch(err => {
