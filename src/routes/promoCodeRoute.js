89b5f8c94554bd5ad9d9a1f9bca9f5ae65eb190f
Ramya-rem <ramyasubramaniam7301@gmail.com>
2025-01-20 14:22:05 +0530

    early_earnedreward added

--- src/routes/promoCodeRoute.js
+++ src/routes/promoCodeRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { validatePromocode} = require('../controllers/promoCodeController');
+const { validatePromocode, yearlyEarnedreward} = require('../controllers/promoCodeController');
 const router = express.Router();
 const { commonPayload } = require('../helpers/validation');
 const { celebrate } = require('celebrate');
 
 router.post('/validatePromocode', celebrate(commonPayload), validatePromocode);
+router.post('/earnedreward',  yearlyEarnedreward);
 
 module.exports = router;
