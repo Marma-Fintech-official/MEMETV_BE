8a29f77d0110c1d160e0b6f21e3a0e37655d587f
Rakesh <paviesh@throughbit.com>
2025-01-31 21:34:43 +0530

    ashwin-streak-logics

--- src/routes/userStreakRoute.js
+++ src/routes/userStreakRoute.js
@@ -10,11 +10,6 @@ const {
   taskStreakRewardClaim,
   multiStreakRewardClaim,
   streakOfStreakRewardClaim,
-  updateClaimedLoginDaysArray,
-  updateClaimedWatchDaysArray,
-  updateClaimedReferDaysArray,
-  updateClaimedTaskDaysArray,
-  updateClaimedMultiDaysArray,
   userStreaks,
 } = require("../controllers/userStreakController");
 const { commonPayload } = require("../helpers/validation");
@@ -28,67 +23,31 @@ router.post(
   celebrate(commonPayload),
   loginStreakRewardClaim
 );
-
 router.post(
   "/watchStreakRewardClaim",
   celebrate(commonPayload),
   watchStreakRewardClaim
 );
-
 router.post(
   "/referStreakRewardClaim",
   celebrate(commonPayload),
   referStreakRewardClaim
 );
-
 router.post(
   "/taskStreakRewardClaim",
   celebrate(commonPayload),
   taskStreakRewardClaim
 );
-
 router.post(
   "/multiStreakRewardClaim",
   celebrate(commonPayload),
   multiStreakRewardClaim
 );
-
 router.post(
   "/streakOfStreakRewardClaim",
   celebrate(commonPayload),
   streakOfStreakRewardClaim
 );
-
-router.post(
-  "/updateClaimedLoginDaysArray",
-  celebrate(commonPayload),
-  updateClaimedLoginDaysArray
-);
-
-router.post(
-  "/updateClaimedWatchDaysArray",
-  celebrate(commonPayload),
-  updateClaimedWatchDaysArray
-);
-
-router.post(
-  "/updateClaimedReferDaysArray",
-  celebrate(commonPayload),
-  updateClaimedReferDaysArray
-);
-
-router.post(
-  "/updateClaimedTaskDaysArray",
-  celebrate(commonPayload),
-  updateClaimedTaskDaysArray
-);
-
-router.post(
-  "/updateClaimedMultiDaysArray",
-  celebrate(commonPayload),
-  updateClaimedMultiDaysArray
-);
-
 router.get(
   "/userStreaks/:telegramId",
   celebrate({
@@ -98,6 +57,5 @@ router.get(
   }),
   userStreaks
 );
-
 router.use(errors());
 module.exports = router;
