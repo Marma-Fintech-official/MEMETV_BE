4e2c8dc959458f44ac673d21ee054476677f1364
pavieshkumar12 <dhanushpavi22@gmail.com>
2025-02-11 18:35:20 +0530

    memeHunt-branch

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -116,6 +116,8 @@ const login = async (req, res, next) => {
         })
       }
 
+
+
       // New user registration logic
       user = new User({
         name,
@@ -135,6 +137,18 @@ const login = async (req, res, next) => {
         },
         levelUpRewards: superUser ? newUserRewards : 0 // Adjust levelUpRewards accordingly
       })
+
+      if (extraRewards > 0) {
+        const newEarlyReward = new userReward({
+          category: 'earlyEarnedRewards',
+          date: today,
+          rewardPoints: newUserRewards,
+          userId: user._id,
+          telegramId: user.telegramId
+        })
+        await newEarlyReward.save()
+      }
+      
 
       if (
         (await calculateDayDifference(
